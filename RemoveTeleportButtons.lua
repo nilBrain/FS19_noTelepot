8d92b52b102b4cd23798107560ab5321b374a7cb
nilBrain <46376130+nilBrain@users.noreply.github.com>
2020-01-22 22:35:31 +0100

    Add files via upload
    Fix Teleport Bug by KeyBoard and GamePad

--- RemoveTeleportButtons.lua
+++ RemoveTeleportButtons.lua
@@ -2,21 +2,30 @@
 -- RemoveTeleportButtons
 --
 -- @author  nilBrain
--- @date    04/01/20
+-- @date    22/01/20
+-- Changelog:
+--		
+-- 	v0.1.0.0 (04.01.2020 03:17):
+-- 		- initial fs19
+--
+-- 	v1.0.0.0 (22.01.2020 22:25):
+-- 		- adaptations for the Modhub
 
 RemoveTeleportButtons = {
 	info = {
         Author = "nilBrain",
-        buildID = "04012020:0317.47.A",
+        buildID = "22012020:2230.47.A",
         title = " RemoveTeleportButtons",
         notes = " Remove Teleport Buttons in IngameMenu",
-        date = " 04.01.2020",
+        date = " 22.01.2020",
 	}
 };
 
-print((" ++ %s V.%s (by %s)"):format(RemoveTeleportButtons.info.title, RemoveTeleportButtons.info.buildID, RemoveTeleportButtons.info.Author));
 
 function RemoveTeleportButtons:loadMap(mapFilename)
+	InGameMenuMapFrame.onClickVisitPlace = function() return; end;
+	InGameMenuMapFrame.onClickEnterVehicle = function() return; end;
+    
     g_currentMission.inGameMenu.pageMapOverview.buttonVisitPlace:setDisabled(true);
     g_currentMission.inGameMenu.pageMapOverview.buttonEnterVehicle:setDisabled(true);
 end;
